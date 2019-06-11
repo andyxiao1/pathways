080ee4ea2cfad4cd14511e1b9b5eb65b49ba164b
Andy Xiao <andyxiao@seas.upenn.edu>
2019-06-10 23:40:40 -0400

    Fix heroku settings

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import store from './utils/loadStoreHelper';
 ReactDOM.render(
   <RootContainer store={store} />,
   document.getElementById('root')
-);
+); 
 
 // document.addEventListener('DOMContentLoaded', () => {
 //   ReactDOM.render(
